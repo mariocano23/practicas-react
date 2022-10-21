1a39e452f9f6b82647651aad48d63f663f4eeb91
mariocano23 <mariocano.alu@iespacomolla.es>
2022-10-21 18:39:26 +0200

    je

--- src/practica-3-4-dwc/App.js
+++ src/practica-3-4-dwc/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Contenedor from './practica-3-4-dwc/Contenedor';
-import Cabecera from './practica-3-4-dwc/Cabecera';
-import Peliculero2 from './practica-3-4-dwc/Peliculero2';
-import PiePagina from './practica-3-4-dwc/PiePagina';
-import InfoExtra from './practica-3-4-dwc/InfoExtra';
-import peliculas from './practica-3-4-dwc/peliculas.json';
+import Contenedor from './Contenedor';
+import Cabecera from './Cabecera';
+import Peliculero2 from './Peliculero2';
+import PiePagina from './PiePagina';
+import InfoExtra from './InfoExtra';
+import peliculas from './peliculas.json';
 
 function App() {
   return (
