93eb445b06603c5d8cb73a599459aafcb84fffe6
mariocano23 <mariocano.alu@iespacomolla.es>
2022-10-23 17:40:09 +0200

    Calculadora acabada

--- src/practica-3-5-dwc/Bibliotecas/bibliotecaCalculos.js
+++ src/practica-3-5-dwc/Bibliotecas/bibliotecaCalculos.js
@@ -1,7 +1,8 @@
 "use strict"
 
+import { borrarPantalla } from "./bibliotecaPantalla";
+
 function porciento(pantalla) {
-    console.log(pantalla.current.innerHTML);
     if(!isNaN(pantalla.current.innerHTML/100)){
         pantalla.current.innerHTML=pantalla.current.innerHTML/100;
     } 
@@ -24,30 +25,42 @@ function multiplicar(num1,num2) {
 function dividir(num1,num2) {
     num1=parseFloat(num1);
     num2=parseFloat(num2);
+    if(num2==0){//Comprobación de división entre cero.
+        return "ERROR";
+    }
     return num1/num2;
 }
-function operacion(pantalla) {
+function operacion(pantalla) { //Función que recoje el contenido de la pantalla y hace la operación que se pide.
     let operacion = pantalla.current.innerHTML;
     let resultado=pantalla.current.innerHTML;
     for (let index = 0; index < operacion.length; index++) {
         if(isNaN(operacion[index])&&operacion[index]!="."){
-            switch(operacion[index]){
-                case "+":
-                    resultado=sumar(operacion.slice(0,index),operacion.slice(index+1,operacion.length));
-                    break;
-                case "X":
-                    resultado=multiplicar(operacion.slice(0,index),operacion.slice(index+1,operacion.length));
-                    break;
-                case "/":
-                    resultado=dividir(operacion.slice(0,index),operacion.slice(index+1,operacion.length));
-                    break;
-                case "-":
-                    resultado=restar(operacion.slice(0,index),operacion.slice(index+1,operacion.length));
-                    break;
+            if(index==operacion.length-1){ //Comprobación de si el signo es el último elemento del string.
+             resultado=operacion.slice(0,index);
+            }else{
+                switch(operacion[index]){
+                    case "+":
+                        resultado=sumar(operacion.slice(0,index),operacion.slice(index+1,operacion.length));
+                        break;
+                    case "X":
+                        resultado=multiplicar(operacion.slice(0,index),operacion.slice(index+1,operacion.length));
+                        break;
+                    case "/":
+                        resultado=dividir(operacion.slice(0,index),operacion.slice(index+1,operacion.length));
+                        break;
+                    case "-":
+                        resultado=restar(operacion.slice(0,index),operacion.slice(index+1,operacion.length));
+                        break;
+                }
             }
         }
     }
-    pantalla.current.innerHTML=resultado;
+    if (resultado=="ERROR") {//Comprobación de error.
+        pantalla.current.innerHTML=resultado;
+        setTimeout(borrarPantalla,500,pantalla);
+    }else{
+        pantalla.current.innerHTML=resultado;
+    }
 }
 
 
--- src/practica-3-5-dwc/Bibliotecas/bibliotecaPantalla.js
+++ src/practica-3-5-dwc/Bibliotecas/bibliotecaPantalla.js
@@ -2,18 +2,20 @@
 
 import { operacion } from "./bibliotecaCalculos";
 
-function numeraAPantalla(ref, pantalla) {
+function numeraAPantalla(ref, pantalla) { //Función para escribir un número por pantalla.
     if(pantalla.current.innerHTML=="0"&&ref.current.innerHTML!="."){
         pantalla.current.innerHTML=null;
     }
     pantalla.current.innerHTML+=ref.current.innerHTML;
 }
 
-function operandoAPantalla(ref, pantalla) {
-    if(isNaN(pantalla.current.innerHTML)){
-        operacion(pantalla);
-    }
-        pantalla.current.innerHTML+=ref.current.innerHTML;
+function operandoAPantalla(ref, pantalla) { //Función para escribir un signo de operación por pantalla y hacer la operación de la pantalla si ya hay un signo.
+        if(isNaN(pantalla.current.innerHTML)){
+            operacion(pantalla);
+        }
+            pantalla.current.innerHTML+=ref.current.innerHTML;
+
+    
 }
 
 function borrarPantalla(pantalla) {
